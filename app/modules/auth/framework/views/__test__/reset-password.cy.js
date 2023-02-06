32952fd835e1530fb89099f68b3629ca70134e1f
Acidiney Dias <acidineydias@gmail.com>
2023-02-06 10:32:59 +0100

    chore: add cypress testing

---
+++ app/modules/auth/framework/views/__test__/reset-password.cy.js
@@ -0,0 +1,13 @@
+/// <reference types="cypress" />
+
+describe('ResetPassword', () => {
+  it('should return token not found error', () => {
+    const fakeToken = crypto.randomUUID()
+
+    cy.visit(`/auth/reset/password/${fakeToken}`)
+      .get('input[name="password"]').type('123456789')
+      .get('input[name="confirmPassword"]').type('123456789')
+      .get('button[type="submit"]').click()
+      .get('div.alert-danger').should('be.visible')
+  })
+})
