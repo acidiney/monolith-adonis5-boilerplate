9264f38cdd0d1d5a983ee40cfe887a8c17720a48
Acidiney Dias <acidineydias@gmail.com>
2023-02-08 19:56:10 +0100

    chore: app-status uses i18n and element-ui

--- resources/js/core/components/app-status.vue
+++ resources/js/core/components/app-status.vue
@@ -1,22 +1,26 @@
 <script setup>
+import { useI18n } from 'vue-i18n'
+
 const statusTheme = {
-  approved: "badge-success",
-  active: "badge-success",
-  canceled: "badge-light",
-  pending: "badge-warning",
-  wait_human_validation: "badge-warning",
-  rejected: "badge-danger",
-  submitted: "badge-info",
+  approved: "success",
+  active: "success",
+  canceled: "light",
+  pending: "warning",
+  wait_human_validation: "warning",
+  rejected: "danger",
+  submitted: "info",
 };
 
+const { t } = useI18n()
+
 const fallbackStatusText = {
-  approved: "Aprovado",
-  active: "Activo",
-  canceled: "Cancelado",
-  pending: "Pendente",
-  wait_human_validation: "Aguardando validação de um regulador!",
-  rejected: "Rejeitado",
-  submitted: "Submetido",
+  approved: t('shared.status.approved'),
+  active: t('shared.status.active'),
+  canceled: t('shared.status.canceled'),
+  pending: t('shared.status.pending'),
+  wait_human_validation: t('shared.status.wait_human_validation'),
+  rejected: t('shared.status.rejected'),
+  submitted: t('shared.status.submitted'),
 };
 
 defineProps({
@@ -30,7 +34,5 @@ defineProps({
 </script>
 
 <template>
-  <span :class="['badge', 'text-uppercase', statusTheme[status]]">
-    {{ text || fallbackStatusText[status] }}
-  </span>
+  <el-tag class="ml-2 text-uppercase" :type="statusTheme[status]">{{ text || fallbackStatusText[status] }}</el-tag>
 </template>
