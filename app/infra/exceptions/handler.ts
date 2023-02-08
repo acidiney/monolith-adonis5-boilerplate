fc70ea772e46017b158b2d904dd11f755eac42ec
Acidiney Dias <acidineydias@gmail.com>
2023-02-08 22:35:42 +0100

    chore: add pool

--- app/infra/exceptions/handler.ts
+++ app/infra/exceptions/handler.ts
@@ -22,7 +22,7 @@ export default class ExceptionHandler extends HttpExceptionHandler {
   protected statusPages = {
     '403': 'errors/unauthorized',
     '404': 'errors/not-found',
-    '500..599': 'errors/server-error',
+    '500': 'errors/server-error',
   }
 
   constructor () {
