3ae9259184922907df02e1b66f5a0b00d520529a
Acidiney Dias <acidiney.dias@ideiasdinamicas.com>
2023-02-16 01:00:25 +0100

    test: fix interfaces

--- app/modules/admin/settings/acl/roles-management/usecases/create-role/__test__/factories/make-create-role-repository-stub.ts
+++ app/modules/admin/settings/acl/roles-management/usecases/create-role/__test__/factories/make-create-role-repository-stub.ts
@@ -2,9 +2,9 @@ import {
   CreateRoleWithTransactionRepository,
 } from 'app/modules/admin/settings/acl/roles-management/usecases/create-role/ports'
 import {RoleEntity} from 'app/modules/admin/settings/acl/roles-management/domain/entities/role-entity'
-export const makeCreateRoleRepositoryStub = (): CreateRoleWithTransactionRepository => {
-  return new (class implements CreateRoleWithTransactionRepository {
-    public async persist (_roleEntity: RoleEntity): Promise<void> {
+export const makeCreateRoleRepositoryStub = (): CreateRoleWithTransactionRepository<any> => {
+  return new (class implements CreateRoleWithTransactionRepository<any> {
+    public async persistWithTransaction (_roleEntity: RoleEntity): Promise<void> {
       //
     }
   })()
