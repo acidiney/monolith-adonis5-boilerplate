25990a9f54a0741d1795be1b83cdebd778e9b2ef
Acidiney Dias <acidineydias@gmail.com>
2023-02-20 00:37:53 +0100

    chore: ident

--- app/modules/@shared/framework/main/socket.ts
+++ app/modules/@shared/framework/main/socket.ts
@@ -1,7 +1,16 @@
 export default (socket) => {
   socket.emit('alert', {
-    type: 'info',
-    title: 'new.user.connected',
-    message: 'new.user.connected.on.other.pc',
+    type: 'success',
+    title: '[Sistema] Nova Release',
+    message: 'Foi adicionado o socket.io, para comunicações em tempo real ❤️',
   })
+
+  setTimeout(() => {
+    socket.emit('alert', {
+      type: 'warning',
+      title: '[Sistema] Nova Release',
+      message: `O socket.io foi implementado de forma modular também,
+      e agora faz parte do contrado de uso do boilerplate!`,
+    })
+  }, 1000)
 }
