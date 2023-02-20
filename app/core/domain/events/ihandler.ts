e0abbd72643fe614e9dd8051924efbcf283f3775
Acidiney Dias <acidineydias@gmail.com>
2023-02-20 13:51:51 +0100

    chore(domain): remove userId from IHandler

--- app/core/domain/events/ihandler.ts
+++ app/core/domain/events/ihandler.ts
@@ -1,7 +1,5 @@
-import { UniqueEntityID } from '../unique-entity-id'
 import { IDomainEvent } from './idomain-event'
 
 export interface IHandler<T extends IDomainEvent = IDomainEvent> {
   handle: (event: T) => void
-  userId: () => UniqueEntityID | undefined
 }
