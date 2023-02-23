993a91ac38986fb3f4a8f81971e925969c81e33b
Acidiney Dias <acidiney.dias@ideiasdinamicas.com>
2023-02-23 22:12:20 +0100

    fix(global): issue when webpack updates lost i18n refs

--- app/modules/@shared/framework/main/socket.ts
+++ app/modules/@shared/framework/main/socket.ts
@@ -1,16 +1,4 @@
-export default (socket) => {
-  socket.emit('alert', {
-    type: 'success',
-    title: '[Sistema] Nova Release',
-    message: 'Foi adicionado o socket.io, para comunicações em tempo real ❤️',
-  })
+import { Socket } from 'socket.io'
 
-  setTimeout(() => {
-    socket.emit('alert', {
-      type: 'warning',
-      title: '[Sistema] Nova Release',
-      message: `O socket.io foi implementado de forma modular também,
-      e agora faz parte do contrado de uso do boilerplate!`,
-    })
-  }, 1000)
+export default (socket: Socket) => {
 }
--- resources/js/i18n/index.js
+++ resources/js/i18n/index.js
@@ -1,5 +1,14 @@
-import pt from '../../lang/pt'
+const r = require.context('../../lang', false, /\.json$/)
 
-export default {
-  pt,
-}
+const keys = r.keys()
+  .map((k) => k.replace('./', '').replace('.json', ''))
+
+
+const i18n = {}
+
+
+keys.forEach((p) => {
+  i18n[p] = r(r.keys().find((k) => k.includes(p)))
+})
+
+export default i18n
