0cec36cdc477ddd083833c66f5976de085610b61
Acidiney Dias <acidineydias@gmail.com>
2023-02-27 10:44:32 +0100

    fix: order

---
+++ app/modules/@shared/framework/infra/db/seeders/07-insert-root-user.ts
@@ -0,0 +1,24 @@
+import BaseSeeder from '@ioc:Adonis/Lucid/Seeder'
+import {RoleModel, UserModel} from 'app/modules/@shared/framework/infra/db/models'
+import {StatusEnum} from 'app/modules/@shared/domain/types'
+
+export default class InsertRootUser extends BaseSeeder {
+  public async run () {
+    const role = await RoleModel.findBy('slug', 'root')
+
+    if (!role) {
+      throw new Error('Role "root" not found!')
+    }
+
+    const rootUser = new UserModel()
+
+    rootUser.firstName = 'Root'
+    rootUser.lastName = 'User'
+    rootUser.email = 'root@itgest.co.ao'
+    rootUser.password = '12345678'
+    rootUser.statusId = StatusEnum.ACTIVE
+    rootUser.roleId = role.id
+    rootUser.avatar = 'https://cube.elemecdn.com/0/88/03b0d39583f48206768a7534e55bcpng.png'
+    await rootUser.save()
+  }
+}
