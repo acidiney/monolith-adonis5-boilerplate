067b690718fa594fdeaf08e675c1d97e9cba9542
ernesto.maria <ernestomaria93@gmail.com>
2023-03-15 15:58:19 +0100

    chore(notifications): notification users when seetings was changed

--- app/modules/admin/settings/application-management/framework/main/events.ts
+++ app/modules/admin/settings/application-management/framework/main/events.ts
@@ -1,5 +1,8 @@
 import { EventDispatcher } from 'app/core/domain'
-import { AppSettingModified } from '../../domain'
+import { AppSettingModifiedEvent } from '../../domain'
 import { LogAppSettingListener } from '../infra/listeners/app-setting-change'
+import { NotifyAllUsersThatSettingWasChangedInRealtimeListener } from
+  '../infra/listeners/notify-all-user-that-settings-was-changed'
 
-EventDispatcher.getInstance().register(AppSettingModified.name, new LogAppSettingListener())
+EventDispatcher.getInstance().register(AppSettingModifiedEvent.name, new LogAppSettingListener())
+  .register(AppSettingModifiedEvent.name, new NotifyAllUsersThatSettingWasChangedInRealtimeListener())
