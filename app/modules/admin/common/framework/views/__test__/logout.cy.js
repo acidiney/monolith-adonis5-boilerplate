bcd06c61f2d570bf55ce12f75487cdd9ff4fd888
Acidiney Dias <acidiney.dias@ideiasdinamicas.com>
2023-03-16 09:36:09 +0100

    test(cypress): fix input refs

--- app/modules/admin/common/framework/views/__test__/logout.cy.js
+++ app/modules/admin/common/framework/views/__test__/logout.cy.js
@@ -2,12 +2,20 @@
 
 describe('Logout', function () {
   it('should do logout', () => {
-    cy.visit('/auth/login')
-      .get('input[type="email"]').type('root@itgest.co.ao')
-      .get('input[type="password"]').type('12345678')
-      .get('button[type="submit"]').click()
-      .url().should('include', 'account/dashboard')
-      .get('.user-dropdown').click().get('.button-logout').click()
-      .get('.alert-success').should('be.visible')
+    cy.visit("/auth/login")
+      .get('input[name="username"]')
+      .type("root@itgest.co.ao")
+      .get('input[type="password"]')
+      .type("12345678")
+      .get('button[type="submit"]')
+      .click()
+      .url()
+      .should("include", "account/dashboard")
+      .get(".user-dropdown")
+      .click()
+      .get(".button-logout")
+      .click()
+      .get(".alert-success")
+      .should("be.visible");
   })
 })
--- app/modules/auth/framework/views/__test__/send-reset-password-link.cy.js
+++ app/modules/auth/framework/views/__test__/send-reset-password-link.cy.js
@@ -2,17 +2,22 @@
 
 describe('SendResetPasswordLink', () => {
   it('should send reset password link', () => {
-    cy.visit('auth/reset/password')
-      .get('input[type="email"]').type('root@itgest.co.ao')
-      .get('button[type="submit"]').click()
-      .get('.alert-success').should('be.visible')
+    cy.visit("auth/reset/password")
+      .get('input[name="username"]')
+      .type("root@itgest.co.ao")
+      .get('button[type="submit"]')
+      .click()
+      .get(".alert-success")
+      .should("be.visible");
   })
 
   it('should return error, when username does not exist', () => {
-    cy.visit('auth/reset/password')
-      .get('input[type="email"]').type('invalid@itgest.co.ao')
-      .get('button[type="submit"]').click()
-      .get('.alert-danger')
-      .should('be.visible')
+    cy.visit("auth/reset/password")
+      .get('input[name="username"]')
+      .type("invalid@itgest.co.ao")
+      .get('button[type="submit"]')
+      .click()
+      .get(".alert-danger")
+      .should("be.visible");
   })
 })
